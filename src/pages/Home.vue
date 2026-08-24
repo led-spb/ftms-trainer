ea9559121b122fffefe721c6cbf2587372894697
Aleksey Ponimash <aleksey.ponimash@rt.ru>
2026-08-24 11:59:32 +0300

    minor ui fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -8,6 +8,7 @@
     const router = useRouter()
     const activity = useActivityStore()
     const routeStore = useRoutesStore()
+    const selectedRouteIndex = ref(0)
 
     const debugMode = computed(() => import.meta.env.DEV)
 
@@ -53,7 +54,7 @@
             <UButton icon="i-lucide-route" :variant="displayRoute ? 'solid': 'outline'" size="xl" @click="followRouteClick">Route</UButton>
         </div>
         <div class="mt-4" v-if="displayRoute">
-            <UCarousel arrows dots v-slot="{ item }" :items="routes" @select="(index) => routeStore.activeRoute = routeStore.routes.at(index)">
+            <UCarousel arrows dots v-slot="{ item }" :start-index="selectedRouteIndex" :items="routes" @select="(index) => {selectedRouteIndex=index; routeStore.activeRoute = routeStore.routes.at(index) }">
                 <UForm>
                     <UFormField label="Name" orientation="horizontal" class="mb-1 font-bold">{{ item.name }}</UFormField>
                     <UFormField label="Distance" orientation="horizontal" class="mb-1">{{ (item.distance/1000).toFixed(1) }} km</UFormField>
