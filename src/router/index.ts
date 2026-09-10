8583b9a6635320c929cba3ae39f61d9f3ccf1743
led-spb <led-spb@users.noreply.github.com>
2026-09-10 22:49:53 +0300

    change history mode

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  //history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { path: '/',     name: 'home', component: () => import('../pages/Home.vue'), },
     { path: '/ride', name: 'ride', component: () => import('../pages/Ride.vue'), },
