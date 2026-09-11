0304657e333ff39c58f315751270f1776d2fb8d8
Aleksey Ponimash <aleksey.ponimash@rt.ru>
2026-09-11 14:28:44 +0300

    app toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,37 @@
 <script setup lang="ts">
+  import type { NavigationMenuItem } from '@nuxt/ui';
+  import { computed } from 'vue';
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute()
+
+  const items = computed<NavigationMenuItem[]>(() => [
+    {
+      label: 'Home',
+      to: {name: 'home'},
+      active: route.name == 'home'
+    },
+    {
+      label: 'Ride',
+      to: {name: 'ride'},
+      active: route.name == 'ride'
+    },
+    {
+      label: 'Activities',
+      to: {name: 'activityList'},
+      active: route.name == 'activityList'
+    },
+  ])
 </script>
 
 <template>
   <UApp>
+    <UHeader title="Trainer" mode="drawer" class="h-10">
+      <template #body>
+        <UNavigationMenu :items="items" orientation="vertical" class="-mx-2.5" />
+      </template>      
+    </UHeader>
+
     <UMain  class="mt-4">
       <RouterView/>
     </UMain>
